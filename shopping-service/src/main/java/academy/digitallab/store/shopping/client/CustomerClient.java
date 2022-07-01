ac7b46a8570333f16edc706a804eeff8dbb01cb8
Cristian602 <ricardo.mendez@ucuenca.edu.ec>
2022-07-01 16:47:56 -0500

    Corrigiendo error de hydrix, cambio por resilience 4j

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -5,10 +5,21 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
-@FeignClient(name = "customer-service", path = "/customers", fallback = CustomerHystrixFallbackFactory.class)
-
+@FeignClient(name = "customer-service")
 public interface CustomerClient {
-    @GetMapping(value = "/{id}")
+
+    @GetMapping(value = "/customers/{id}")
+    @CircuitBreaker(name = "customerIns", fallbackMethod = "getFallbackCustomer")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+
+    default ResponseEntity<Customer> getFallbackCustomer(RuntimeException e) {
+        Customer customer = Customer.builder()
+                .firstName("none")
+                .lastName("none")
+                .email("none")
+                .photoUrl("none").build();
+        return ResponseEntity.ok(customer);
+    }
 }
--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/ProductClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/ProductClient.java
@@ -1,17 +1,25 @@
 package academy.digitallab.store.shopping.client;
 
+import academy.digitallab.store.shopping.model.Customer;
 import academy.digitallab.store.shopping.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 @FeignClient(name = "product-service", path = "/products")
+
 public interface ProductClient {
+
     @GetMapping(value = "/{id}")
+    @CircuitBreaker(name = "productIns", fallbackMethod = "getFallbackProduct")
     public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
+
     @GetMapping(value = "/{id}/stock")
     public ResponseEntity<Product> updateStockProduct(@PathVariable  Long id ,@RequestParam(name = "quantity", required = true) Double quantity);
+
+    default ResponseEntity<Product> getFallbackProduct(RuntimeException e) {
+        return ResponseEntity.ok(null);
+    }
 }
